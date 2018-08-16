f798a61f3e4cc5f954a6bdb4343ad443753e8889
Pakkapon Phongthawee <me@pureapp.in.th>
2018-08-16 17:27:00 +0700

    update

---
+++ arrayfire-inpaint-multires/arrayfire-inpaint-multires/wSolver.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "stdafx.h"
+#include "Gradient.h"
+#include "Signum.h"
+
+//w = max(abs(gradU + b) - (1 / theta), 0) * signnum(gradU+b);
+
+
+array wSolver(array u, array b, double theta) {	
+	int height = u.dims(0);
+	int width = u.dims(1);
+	array gradB = Gradient(u) + b;
+	array w = abs(gradB) - (1 / theta);
+	array zeroMatrix = constant(0, height * 2, width, f64);
+	w = max(w, zeroMatrix) * Signum(gradB);
+	return w;
+}
+
